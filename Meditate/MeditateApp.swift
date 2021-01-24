251235d60c6abf06f783fad78913ed8ba79d0994
Duff Neubauer <d.neubauer@me.com>
2021-01-24 07:42:57 -0800

    Initial Commit

---
+++ Meditate/MeditateApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeditateApp.swift
+//  Meditate
+//
+//  Created by Duff Neubauer on 1/24/21.
+//
+
+import SwiftUI
+
+@main
+struct MeditateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
